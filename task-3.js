30c9327367a6580cedb8401f244554d0af79c35b
Andreas-206 <puma_real@ukr.net>
2023-10-30 15:23:44 +0200

    task-3

--- task-3.js
+++ task-3.js
@@ -1,7 +1,16 @@
-
-
-
-
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername: function (newName) {
+        this.username = newName;
+    },
+    updatePlayTime: function (hours) {
+        this.playTime += hours;
+    },
+    getInfo: function () {
+        return `${this.username} has ${this.playTime} active hours!`;
+    },
+};
 
 
 
